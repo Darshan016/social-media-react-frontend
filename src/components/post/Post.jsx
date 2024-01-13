cf11c68812f49490fdf5745307cf855b1bb52177
Darshan Panchal <panchaldarshan121@gmail.com>
2024-01-13 15:10:10 +0530

    fetching post details dynamically

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,36 +1,38 @@
 import "./post.css";
 import { MoreVert } from "@mui/icons-material";
+import {Users} from '../../DummyData'
 
-export default function Post() {
+export default function Post({post}) {
+  console.log(post)
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
             <img
-              src="/assets/persons/itachi.jpg"
+              src={Users.filter((u)=>u.id===post?.userId)[0].profilePicture}
               alt=""
               className="postProfile"
             />
-            <span className="postUserName">Itachi Uchiha</span>
-            <span className="postDate">1 Year Ago</span>
+            <span className="postUserName">{Users.filter((u)=>u.id===post?.userId)[0].userName}</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-            <span className="postText">Test post</span>
-            <img src="/assets/posts/scene1.jpg" alt="" className="postImg" />
+            <span className="postText">{post?.desc}</span>
+            <img src={post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
             <div className="postBottomLeft">
                 <img src="assets/like.png" alt="" className="likeIcon" />
                 <img src="assets/heart.png" alt="" className="likeIcon" />
-                <span className="postLikeCounter">50 users liked it.</span>
+                <span className="postLikeCounter">{post?.like} users liked it</span>
             </div>
             <div className="postBottomRight">
-                <span className="postCommentText">40 comments</span>
+                <span className="postCommentText">{post?.comment} comments</span>
             </div>
         </div>
       </div>
