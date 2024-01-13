ade3fc6340eee4df7f7faad22e754accf27ec33e
Darshan Panchal <panchaldarshan121@gmail.com>
2024-01-13 15:20:23 +0530

    dummy data for users and posts

---
+++ src/DummyData.js
@@ -0,0 +1,107 @@
+export const Users=[
+    {
+        id:1,
+        profilePicture:'assets/persons/askellad.jpg',
+        userName:'Askellad'
+
+    },
+    {
+        id:2,
+        profilePicture:'assets/persons/hachiman.jpg',
+        userName:'Hikigaya'
+
+    },
+    {
+        id:3,
+        profilePicture:'assets/persons/itachi.jpg',
+        userName:'Itachi Uchiha'
+
+    },
+    {
+        id:4,
+        profilePicture:'assets/persons/kiyotaka.jpg',
+        userName:'Ayanokoji Kiyotaka'
+
+    },
+    {
+        id:5,
+        profilePicture:'assets/persons/lelouch.jpg',
+        userName:'Lelouch'
+
+    },
+    {
+        id:6,
+        profilePicture:'assets/persons/nine.jpg',
+        userName:'Nine'
+
+    },
+    {
+        id:7,
+        profilePicture:'assets/persons/oreki.jpg',
+        userName:'Hotarou'
+
+    },
+    {
+        id:8,
+        profilePicture:'assets/persons/shadow.jpg',
+        userName:'Cid'
+
+    },
+]
+
+export const Posts=[
+    {
+        id:1,
+        desc:'demo desc',
+        photo:'assets/posts/scene1.jpg',
+        date:'5 mins ago',
+        userId:1,
+        like:32,
+        comment:9
+    },
+    {
+        id:2,
+        desc:'demo desc',
+        photo:'assets/posts/scene2.jpg',
+        date:'5 mins ago',
+        userId:2,
+        like:32,
+        comment:9
+    },
+    {
+        id:3,
+        desc:'demo desc',
+        photo:'assets/posts/scene3.jpg',
+        date:'5 mins ago',
+        userId:3,
+        like:32,
+        comment:9
+    },
+    {
+        id:4,
+        desc:'demo desc',
+        photo:'assets/posts/scene4.jpg',
+        date:'5 mins ago',
+        userId:4,
+        like:32,
+        comment:9
+    },
+    {
+        id:5,
+        desc:'demo desc',
+        photo:'assets/posts/scene5.jpg',
+        date:'5 mins ago',
+        userId:5,
+        like:32,
+        comment:9
+    },
+    {
+        id:6,
+        desc:'demo desc',
+        photo:'assets/posts/scene6.jpg',
+        date:'5 mins ago',
+        userId:6,
+        like:32,
+        comment:9
+    }
+]
